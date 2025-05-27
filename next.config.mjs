53e3fa82e36d696f7ba5ed14237fb14845fe7f60
Keasanda <mlungisi716@gmail.com>
2025-05-27 12:37:54 +0200

    Next .config chnage

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-<<<<<<< HEAD
   basePath: '/logistics-ai-website',
   trailingSlash: true
 };
-=======
-
-  output: 'export',
-}
->>>>>>> 5c346a7a6f145d53014f4b9513ccc317ae91f40a
 
 module.exports = nextConfig;
