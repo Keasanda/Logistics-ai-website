dbaf2fbcc61bc8f5dab9059a99bf63814a51896a
Keasanda <mlungisi716@gmail.com>
2025-05-27 12:32:59 +0200

    Good change

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
-}
+  basePath: '/logistics-ai-website',
+  trailingSlash: true
+};
 
-export default nextConfig
+module.exports = nextConfig;
